4d3d33e73ad249f048c7cf5b0117490ecbefe92c
Rohit Musti <rmusti@redhat.com>
2019-06-13 14:11:27 -0400

    added a config file

---
+++ config.py
@@ -0,0 +1,17 @@
+class data():
+    def __init__(self, folder="./data/"):
+        self.folder = folder
+        # training data
+        # - usually used when you want to "officially" train your model
+        self.train_folder = self.folder + "train/"
+        self.train_data_orig = self.train_folder + "train-v2.0.json"
+        self.train_data_exp2 = self.train_folder + "train-exp2.json"
+        # development data
+        #  - usually reserved for hypertuning or for training locally to ensure models work
+        self.dev_folder = self.folder + "dev/"
+        self.dev_data_orig = self.dev_folder + "dev-v2.0.json"
+        self.dev_data_exp2 = self.dev_folder + "dev-exp2.json"
+        # word embeddings
+        self.embed_folder = self.folder + "embeddings/"
+        self.word_embeddings = self.embed_folder + "glove.840B.300d.txt"
+        self.char_embeddings = self.embed_folder + "glove.840B.300d-char.txt"

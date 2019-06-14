22442124ff6ad361f10415e40c424640140659e9
Rohit Musti <rmusti@redhat.com>
2019-06-14 09:55:24 -0400

    got rid of download stuff

--- exp3_setup.py
+++ exp3_setup.py
@@ -26,60 +26,6 @@ from tqdm import tqdm
 from zipfile import ZipFile
 
 
-def download_url(url, output_path, show_progress=True):
-    """
-    unchanged from @chrischute
-    """
-    class DownloadProgressBar(tqdm):
-        def update_to(self, b=1, bsize=1, tsize=None):
-            if tsize is not None:
-                self.total = tsize
-                self.update(b * bsize - self.n)
-
-            if show_progress:
-                # Download with a progress bar
-                with DownloadProgressBar(unit='B', unit_scale=True,
-                             miniters=1, desc=url.split('/')[-1]) as t:
-                    urllib.request.urlretrieve(url,
-                                   filename=output_path,
-                                   reporthook=t.update_to)
-            else:
-            # Simple download with no progress bar
-                urllib.request.urlretrieve(url, output_path)
-
-
-def url_to_data_path(url):
-    """
-    unchanged from @chrischute
-    """
-    return os.path.join('./data/', url.split('/')[-1])
-
-
-def download(args):
-    """
-    unchanged from @chrischute
-    """
-
-        # Can add other downloads here (e.g., other word vectors)(
-    downloads = [('GloVe word vectors', args.glove_url),]
-
-    for name, url in downloads:
-        output_path = url_to_data_path(url)
-        if not os.path.exists(output_path):
-            print('Downloading {}...'.format(name))
-            download_url(url, output_path)
-
-        if os.path.exists(output_path) and output_path.endswith('.zip'):
-            extracted_path = output_path.replace('.zip', '')
-            if not os.path.exists(extracted_path):
-                print('Unzipping {}...'.format(name))
-                with ZipFile(output_path, 'r') as zip_fh:
-                    zip_fh.extractall(extracted_path)
-
-    print('Downloading spacy language model...')
-    run(['python', '-m', 'spacy', 'download', 'en'])
-
-
 def word_tokenize(sent):
     """
     unchanged from @chrischute

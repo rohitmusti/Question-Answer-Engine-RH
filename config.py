958d576057a1cd45ca003c32c11012bf80ff3b26
Rohit Musti <rmusti@redhat.com>
2019-06-18 08:38:55 -0400

    updated config

--- config.py
+++ config.py
@@ -9,15 +9,27 @@ class data():
         self.train_data_exp3 = self.train_folder + "train-exp3.json"
         self.train_eval_exp2 = self.train_folder + "train-eval-exp2.json"
         self.train_eval_exp3 = self.train_folder + "train-eval-exp3.json"
+
         # development data
         #  - usually reserved for hypertuning or for training locally to ensure models work
         self.dev_folder = self.folder + "dev/"
         self.dev_data_orig = self.dev_folder + "dev-v2.0.json"
         self.dev_data_exp2 = self.dev_folder + "dev-exp2.json"
         self.dev_data_exp3 = self.dev_folder + "dev-exp3.json"
+        self.dev_eval_exp2 = self.train_folder + "dev-eval-exp2.json"
+        self.deb_eval_exp3 = self.train_folder + "dev-eval-exp3.json"
+
         # word embeddings
         self.embed_folder = self.folder + "embeddings/"
         self.word_embeddings = self.embed_folder + "glove.840B.300d.txt"
         self.char_embeddings = self.embed_folder + "glove.840B.300d-char.txt"
+        self.word2idx_file = self.embed_folder + "word2idx.json"
+        self.char2idx_file = self.embed_folder + "char2idx.json"
+        self.word_emb_file = self.embed_folder + "word_emb.json"
+        self.dev_emb_file = self.embed_folder + "deb_emb.json"
 
+        # random seed for consistent runs
         self.random_seed = 3716
+
+        # save file to save logs
+        self.logging_dir = "./save/"

b5e07fb3578f95370c395a73b9e137549d207f25
Rohit Musti <rohit.musti.rm@gmail.com>
2019-06-20 15:31:11 -0400

    it is training now, running into memory issues though, working on a slightly smaller dataset

--- util.py
+++ util.py
@@ -56,12 +56,22 @@ class SQuAD(data.Dataset):
             # SQuAD 2.0: Use index 0 for no-answer token (token 1 = OOV)
             batch_size, c_len, w_len = self.context_char_idxs.size()
             ones = torch.ones((batch_size, 1), dtype=torch.int64)
+            self.context_idxs = torch.unsqueeze(self.context_idxs, 1).t()
             self.context_idxs = torch.cat((ones, self.context_idxs), dim=1)
+
+            batch_size, q_len = self.question_idxs.size()
+            ones = torch.ones((batch_size, 1), dtype=torch.int64)
             self.question_idxs = torch.cat((ones, self.question_idxs), dim=1)
 
+            batch_size, c_len, w_len = self.context_char_idxs.size()
             ones = torch.ones((batch_size, 1, w_len), dtype=torch.int64)
             self.context_char_idxs = torch.cat((ones, self.context_char_idxs), dim=1)
-            self.question_char_idxs = torch.cat((ones, self.question_char_idxs), dim=1)
+
+            batch_size, q_len, topics_len = self.question_char_idxs.size()
+            ones = torch.ones((batch_size, 1, w_len), dtype=torch.int64)
+            print("ones size:", ones.shape)
+            print("question_char_size",self.question_char_idxs.size())
+#            self.question_char_idxs = torch.cat((ones, self.question_char_idxs), dim=1)
 
             self.y1s += 1
             self.y2s += 1

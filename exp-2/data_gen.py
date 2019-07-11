1d2b89c154b172f79b9498cd60e6d678bf4756ea
Rohit Musti <rohit.musti.rm@gmail.com>
2019-07-11 11:37:15 -0400

    replaced toy_gen with data_gen

--- exp-2/data_gen.py
+++ exp-2/data_gen.py
@@ -6,9 +6,11 @@ author: @rohitmusti
 import ujson as json
 from tqdm import tqdm
 from toolkit import save, quick_clean, get_logger
+import sys
+from random import randrange
 import config
 
-def toy_transformer(in_file, out_file_1, out_file_2,  topic_num, paragraph_num, qas_num, logger):
+def toy_transformer(in_file, out_file_1, out_file_2, out_file_3, topic_num, logger):
     """
     distill original data into at most 15 topics, with each having at most 5 paragraphs,
     each of which has 5 questions and 5 answers
@@ -19,11 +21,13 @@ def toy_transformer(in_file, out_file_1, out_file_2,  topic_num, paragraph_num, 
     return:
         none, the data is written to an output
     """
-    logger.info(f"This toy data set will be compromised of {topic_num} topics, each containing {paragraph_num} paragraphs, which each contain {qas_num} qas")
+    logger.info(f"This toy data set will be compromised of {topic_num} topics")
     new_data = {}
     new_data['experiment'] = "toy"
     new_dev_data = {}
     new_dev_data['experiment'] = "toy_dev"
+    new_test_data = {}
+    new_test_data['experiment'] = "toy_train"
     with open(in_file, "r") as fh:
         logger.info(f"Importing: {in_file}")
         source = json.load(fh)
@@ -32,18 +36,18 @@ def toy_transformer(in_file, out_file_1, out_file_2,  topic_num, paragraph_num, 
         new_data["data"] = []
         new_dev_data["version"] = source["version"]
         new_dev_data["data"] = []
+        new_test_data["version"] = source["version"]
+        new_test_data["data"] = []
         topic_counter = topic_num
         for topic in tqdm(source["data"]):
             logger.info(f"Processing: {topic['title']}")
             topic_dict = {}
             topic_dict["title"] = topic["title"]
             topic_dict["paragraphs"] = []
-            para_counter = paragraph_num
             for para in topic["paragraphs"]:
                 paragraph = {}
                 paragraph["context"] = para["context"]
                 paragraph["qas"] = []
-                qa_counter = qas_num
                 for qas in para['qas']:
                     qas_dict = {}
                     qas_dict["id"] = qas["id"]
@@ -57,20 +61,14 @@ def toy_transformer(in_file, out_file_1, out_file_2,  topic_num, paragraph_num, 
                             answer_dict["text"] = answer["text"]
                             qas_dict["answers"].append(answer_dict)
                     paragraph["qas"].append(qas_dict)
-                    qa_counter -= 1
-                    if qa_counter == 0:
-                        break
-
                 topic_dict["paragraphs"].append(paragraph)
-                para_counter -= 1
-                if para_counter == 0:
-                    break
-
             if topic_counter >= 0:
                 new_data["data"].append(topic_dict)
-            else:
+            elif topic_counter >= -20:
                 new_dev_data["data"].append(topic_dict)
-            if topic_counter == -5:
+            else:
+                new_test_data["data"].append(topic_dict)
+            if topic_counter == -30:
                 break
             topic_counter -= 1
 
@@ -78,12 +76,16 @@ def toy_transformer(in_file, out_file_1, out_file_2,  topic_num, paragraph_num, 
     save(filename=out_file_1, obj=new_data)
     logger.info(f"Saving new dev data to {out_file_2}")
     save(filename=out_file_2, obj=new_dev_data)
-
-
+    logger.info(f"Saving new test data to {out_file_3}")
+    save(filename=out_file_3, obj=new_test_data)
 
 if __name__ == "__main__":
     c = config.config()
-    log = get_logger(log_dir=c.logging_dir, name="toy-gen")
-    toy_transformer(in_file=c.train_data_orig, out_file_1=c.toy_data_orig, 
-                    out_file_2=c.toy_dev_data_orig, topic_num=5, 
-                    paragraph_num=5, qas_num=5, logger=log)
+    flags = sys.argv
+    topic_num = int(flags[1])
+    log = get_logger(log_dir=c.logging_dir, name="data-gen")
+    toy_transformer(in_file="data/train/orig-train-v2.0.json", 
+                    out_file_1="data/train/train-v2.0.json", 
+                    out_file_2="data/dev/dev-v2.0.json", 
+                    out_file_3="data/test/test-v2.0.json", 
+                    topic_num=topic_num,logger=log)

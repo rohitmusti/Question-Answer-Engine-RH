f34c51e0cd5253d42ca04bb2d6164e2ee0bb09b7
Rohit Musti <rohit.musti.rm@gmail.com>
2019-07-22 16:28:25 -0400

    end 2 end should work now

--- exp-1/e2e_test.sh
+++ exp-1/e2e_test.sh
@@ -1,4 +1,5 @@
 python3 data_gen.py --train_topic_num 39 --dev_topic_num 10 --test_topic_num 9
 python3 exp1_transform.py --datasplit all
 python3 exp1_setup.py --chunk_size 10
-# python3 exp1_train.py --eval_steps 4000 --num_epochs 5 
+python3 exp1_train.py --eval_steps 4000 --num_epochs 5 --num_train_chunks 142 --num_dev_chunks 60
+

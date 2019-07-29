aa8987dc23a6d4fb7f6a072327cfa6cc82e6ef98
Rohit Musti <rohit.musti.rm@gmail.com>
2019-07-29 12:17:21 -0400

    added some args, need to comment them

--- exp-3/args.py
+++ exp-3/args.py
@@ -1,5 +1,35 @@
 import argparse
 
+def get_exp3_featurize_args():
+    parser = argparse.ArgumentParser("Arguments for featurizing data in prep for training")
+    _add_common_exp3_args(parser)
+
+    parser.add_argument("--ques_limit",
+                        type=int,
+                        default=40,
+                        help="The max number of words to keep from a question.")
+    parser.add_argument("--glove_file",
+                        type=str,
+                        default="./data/glove.840.300d.txt",
+                        help="The file containing the word vectors.")
+    
+    args = parser.parse_args()
+    args.train_in_file = "./data/clean-train-exp3.json"
+    args.train_feature_file = "./data/train-prepped-exp3.npz"
+    args.train_topic_title_id_map_file = "./data/train-title-topic-id-map.json"
+    args.train_eval_examples_file = "./data/train-eval-examples.json"
+    args.train_feature_file = "./data/train-features.npz"
+    args.dev_in_file = "./data/clean-dev-exp3.json"
+    args.dev_feature_file = "./data/dev-prepped-exp3.npz"
+    args.dev_topic_title_id_map_file = "./data/dev-title-topic-id-map.json"
+    args.dev_eval_examples_file = "./data/dev-eval-examples.json"
+
+    args.word_emb_file = "./data/word-emb.json"
+    else:
+        raise ValueError(f'Unrecognized metric name: "{args.data_split}"')
+
+    return args
+
 def get_exp3_transformer_args():
     parser = argparse.ArgumentParser("Arguments for transforming data into a useful form for classification")
     _add_common_exp3_args(parser)
@@ -19,4 +49,9 @@ def _add_common_exp3_args(parser):
     parser.add_argument("--logging_dir",
                         type=str,
                         default="./logs/",
+                        help="The folder where all logs are stored")
+    parser.add_argument("--data_split",
+                        type=str,
+                        default="train",
+                        choices=("train", "test"),
                         help="The folder where all logs are stored")

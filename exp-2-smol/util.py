fdcc0d2c946f688fffaec69ddcba7e24f9e039ec
Rohit Musti <rohit.musti.rm@gmail.com>
2019-08-06 11:24:17 -0400

    updated collate function for 6 vals

--- exp-2-smol/util.py
+++ exp-2-smol/util.py
@@ -145,20 +145,19 @@ def collate_fn(examples):
         return padded
 
     # Group by tensor type
-    context_idxs, context_char_idxs, \
+    context_idxs,  \
         question_idxs, question_char_idxs, \
         y1s, y2s, ids = zip(*examples)
 
     # Merge into batch tensors
     context_idxs = merge_1d(context_idxs)
-    context_char_idxs = merge_2d(context_char_idxs)
     question_idxs = merge_1d(question_idxs)
     question_char_idxs = merge_2d(question_char_idxs)
     y1s = merge_0d(y1s)
     y2s = merge_0d(y2s)
     ids = merge_0d(ids)
 
-    return (context_idxs, context_char_idxs,
+    return (context_idxs, 
             question_idxs, question_char_idxs,
             y1s, y2s, ids)
 

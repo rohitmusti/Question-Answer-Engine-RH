733ddeff0f53e2849c1935e3a93a9248077f184c
Rohit Musti <rohit.musti.rm@gmail.com>
2019-08-08 10:31:29 -0400

    updated

--- exp-4/rf_trainer.py
+++ exp-4/rf_trainer.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import ujson as json
 from tqdm import tqdm
+from sklearn.ensemble import RandomForestClassifier
+
 from args import get_exp3_train_args
 
 def main(args):
@@ -11,21 +13,24 @@ def main(args):
     with open(args.word_emb_file, 'r') as fh:
         word_vectors = (json.load(fh))
     
+    print('retrieving embeddings for train...')
     t_x = []
     for word_array in t_raw["qw_idxs"]:
         nwa = []
         for word in word_array:
             nwa.append(word_vectors[word])
-        t_x.append(nwa)
+        t_x.append(np.asarray(nwa))
 
+    print('retrieving embeddings for dev...')
     d_x = []
     for word_array in d_raw["qw_idxs"]:
         nwa = []
         for word in word_array:
             nwa.append(word_vectors[word])
-        d_x.append(nwa)
+        d_x.append(np.asarray(nwa))
 
 
+    print('creating dataframes...')
     t_X = pd.DataFrame(t_x,
                       columns=[str(i+1) for i in range(31)])
     d_X = pd.DataFrame(np.array(d_raw['qw_idxs']),
@@ -33,6 +38,7 @@ def main(args):
     t_Y = pd.DataFrame(t_raw['topic_ids'], columns=['topic_ids'])
     d_Y = pd.DataFrame(d_raw['topic_ids'], columns=['topic_ids'])
 
+    print('training...')
     forest = RandomForestClassifier(n_estimators = 100)
     forest = forest.fit(t_X, t_Y)
 
